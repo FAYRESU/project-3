0c61eb342583f0d374e339b400e7dc7dcfb5c54f
FAYRESU <136466571+FAYRESU@users.noreply.github.com>
2024-09-12 23:32:00 +0700

    product_model.php

--- application/models/product_model.php
+++ application/models/product_model.php
@@ -1,40 +1,37 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-class Customer_model extends CI_Model {
+defined('BASEPATH') or exit('No direct script access allowed');
+class Product_model extends CI_Model
+{
+	public function getAll()
+	{
+		$sql = "SELECT * FROM producter";
+		$result = $this->db->query($sql)->result();
+		return $result;
+	}
 
-		public function getAll()
-        {
-			$sql = "SELECT * FROM products ";
-            $result = $this->db->query($sql)->result();
-            return $result;
-		}
+	public function getAllv2()
+	{
+		$result = $this->db->get('producter')->result();
+		return $result;
+	}
 
-        public function getAllv2()
-        {
-            $result = $this->db->query($sql)->result();
-            return $result;
-		}
+	public function getByCode($cd)
+	{
+		$sql = "SELECT * FROM producter WHERE code='$cd'";
+		$result = $this->db->query($sql)->result();
+		return $result;
+	}
 
-        public function getByCode($cd)
-        {
-			$sql = "SELECT * FROM products WHERE code='$cd'";
-            $result = $this->db->query($sql)->result();
-            return $result;
-		}
+	public function getByCodev2($cd)
+	{
+		$this->db->where('code', $cd);
+		$result = $this->db->get('producter')->result();
+		return $result;
+	}
 
-         public function getByCodev2($cd)
-        {
-           
-            $this->db->where('code', $cd);
-            $result = $this->db->get('products')->result();
-            // $result = $this->db->query($sql)->result();
-            return $result;
-		}
-
-
-         public function removeByCode($cd)
-        {
-			$sql = "DELETE FROM products WHERE code='$cd";
-           $this->db->query($sql);
-		}
+	public function removeByCode($cd)
+	{
+		$sql = "DELETE FROM producter WHERE code='$cd'";
+		$this->db->query($sql);
+	}
 }
